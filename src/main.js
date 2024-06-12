1a0ba085783f5cd4a02cb3aabde3c5c3f428a479
Abdullah Yahya <ayahya@qualys.com>
2024-06-11 19:37:03 -0700

    improved sticky header code

--- src/main.js
+++ src/main.js
@@ -156,19 +156,21 @@
 }());
 
 /*
- * add class to header when user scrolls down x pixels
+ * add class to header when user scrolls down x pixels to stick header to top
  */
 (function () {
-	new Waypoint({
-		element: document.getElementById('header'),
-		handler: function(direction) {
-			if (direction === "down") {
-				$("header").addClass("stuck");
-			} else {
-				$("header").removeClass("stuck");
-			}
-			console.log(direction);
-		},
-		offset: -20
-	})
+	var offset = $(".banner-and-header").offset();
+	checkOffset();
+
+	$(window).scroll(function() {
+		checkOffset();
+	});
+
+	function checkOffset() {
+		if ( $(document).scrollTop() > offset.top){
+			$('.banner-and-header').addClass('stuck');
+		} else {
+			$('.banner-and-header').removeClass('stuck');
+		} 
+	}
 }());
