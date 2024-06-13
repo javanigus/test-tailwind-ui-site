162f9af1562ad7b9d4a2e4260390b4b55875dc14
Abdullah Yahya <ayahya@qualys.com>
2024-06-13 09:09:00 -0700

    Updates

---
+++ index.1d78987f.js
@@ -0,0 +1,2 @@
+!function(){var n=$(".banner-and-header").offset();function a(){$(document).scrollTop()>n.top?$(".banner-and-header").addClass("stuck"):$(".banner-and-header").removeClass("stuck")}a(),$(window).scroll(function(){a()})}();
+//# sourceMappingURL=index.1d78987f.js.map
