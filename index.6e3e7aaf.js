fc0ce97568847c9a05b312092600d5895c58a1ea
Abdullah Yahya <ayahya@qualys.com>
2024-06-13 09:45:49 -0700

    Updates

--- index.6e3e7aaf.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",autoplay:{delay:3e3},speed:400,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.6e3e7aaf.js.map
---
+++ index.7f3e4c65.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",autoplay:{delay:3e3,disableOnInteraction:!1,pauseOnMouseEnter:!0},speed:400,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.7f3e4c65.js.map
